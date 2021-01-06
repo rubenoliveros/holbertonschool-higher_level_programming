94c8953ca7567103a7be8544fbd8e59623c5cadb
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-06 03:23:09 +0000

    Adding docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""a class Square that defines a square by: (based on 0-square.py)"""
+"""A class Square that defines a square by: (based on 0-square.py)"""
 
 
 class Square:
+    """Declares the class Square"""
     def __init__(self, size):
+        """Initializates tha class"""
         self.__size = size
