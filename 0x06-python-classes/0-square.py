5284c383a355d6afe75adfe45916e25d07c2e344
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-06 02:30:15 +0000

    Fixing blank lines

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-An empty class Square that defines a square
-"""
+"""An empty class Square that defines a square"""
 
 
 class Square():
-    """
-    An empty Square class
-    """
+    """An empty Square class """
     pass
