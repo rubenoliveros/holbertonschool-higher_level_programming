2b3940727eff41e0cee05d5357cdc4e01a376c59
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-06 03:54:54 +0000

    Adding docstrings

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A class Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """Declares the class Square"""
+    def __init__(self, size=0):
+        """Initializates tha class"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Defines the class area"""
+        return (self.__size) ** 2
