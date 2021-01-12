a3b93389ef3cd96b13c3d9f27d8f49a489b56ec7
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-12 00:31:26 +0000

    Fixing for pep8

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -40,13 +40,13 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width == 0  or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * self.__width + 2 * self.__height
 
     def __str__(self):
         pixel = ""
-        if self.__width == 0  or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return pixel
         for i in range(self.__height):
             for j in range(self.__width):
@@ -54,4 +54,3 @@ class Rectangle:
             if i is not self.__height:
                 pixel += "\n"
         return pixel
-         
