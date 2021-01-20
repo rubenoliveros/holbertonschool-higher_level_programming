cc8bd013b4a47a7936d3cdd07bf6cdf2f0271708
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-20 23:30:53 +0000

    Fixing docstring

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,5 +9,5 @@ def append_write(filename="", text=""):
     """
     Defines a function that appends a string at the end of a text file
     """
-    with open(filename, mode = "a", encoding="utf-8") as f:
+    with open(filename, mode="a", encoding="utf-8") as f:
         return f.write(text)
