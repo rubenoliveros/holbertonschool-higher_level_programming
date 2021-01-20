aaaaf5c616ca3e151c8fd6676ffab3216bee9006
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-20 04:07:22 +0000

    Fixing docstring

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -9,4 +9,7 @@ class BaseGeometry:
     A class with area attribute that raises an exception 
     """
     def area(self):
+        """
+        Raises an exception
+        """
         raise Exception("area() is not implemented")
