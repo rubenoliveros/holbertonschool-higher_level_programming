4d41bff2e47746544fd36e8af631b197f323f9a1
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-20 23:35:50 +0000

    Fixing docstring

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A function that returns the JSON representation of an object (string):
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Define a function that returns a JSON representation
+    """
+    return json.dumps(my_obj)
