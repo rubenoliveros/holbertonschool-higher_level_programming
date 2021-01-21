f8374f04b12484008369bb6e27e4cf9b51c0c736
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-21 03:24:35 +0000

    First version

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ and then save them to a file:
 """
 
 from sys import argv
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
