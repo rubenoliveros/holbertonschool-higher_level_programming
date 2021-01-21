0595a25886503324ab8c412e29175763cfe592ea
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-01-21 04:49:22 +0000

    First version

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+A class Student that defines a student by:
+"""
+
+
+class Student:
+    """
+    Declares a class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the student class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
+
+        def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        """
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except:
+                pass
