7146a0bf6bdfa216d528f71bbc37b7b082afb042
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-03-22 17:27:02 -0500

    Switching the if statement

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (!process.argv[2]) {
-  console.log('Missing size');
-} else {
+if (process.argv[2] && Number(process.argv[2])) {
   for (let x = 0; x < process.argv[2]; x++) {
     console.log('X'.repeat(process.argv[2]));
   }
+} else {
+  console.log('Missing size');
 }
