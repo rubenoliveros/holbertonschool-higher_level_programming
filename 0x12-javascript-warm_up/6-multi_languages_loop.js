a9eb34a89876b0a0e98f153a44650ca571bb4069
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-03-22 16:22:58 -0500

    First version

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const i in array) {
-  console.log(array[i]);
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const i in arr) {
+  console.log(arr[i]);
 }
