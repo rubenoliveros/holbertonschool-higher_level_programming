132a27b6590949fea2d5afb7766380f8678dec3c
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-03-22 18:41:29 -0500

    Adding Number to output

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!process.argv[3]) {
+if (!process.argv[3].length <= 3) {
   console.log(0);
 } else {
-  console.log(process.argv.sort().reverse()[1]);
+  console.log(Number(process.argv.sort().reverse()[1]));
 }
