7bdfd1b83fb08e7aeda27de59bb785215afa8166
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-03-27 22:47:09 -0500

    First version

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,14 +17,16 @@ of hbtn_0e_0_usa where name matches the argument.
 
 import MySQLdb
 from sys import argv
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(argv[4]))
-    rows = cursor.fetchall()
-    for row in rows:
+    db = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3])
+    cursor = states.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
         if row[1] == argv[4]:
-            print(row)
+            print("{}".format(row))
     cursor.close()
     db.close()
