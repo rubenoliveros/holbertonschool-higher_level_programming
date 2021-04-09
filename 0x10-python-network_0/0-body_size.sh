c886614e64988c7a661fb7f397ca7785be5755f4
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-04-08 19:08:24 -0500

    Fixing for length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays
-# the size of the body of the response
-# The size must be displayed in bytes
-# You have to use curl
-
 curl -Is "$1" | grep 'Content-Length' | awk 'NF>1{print $NF}'
