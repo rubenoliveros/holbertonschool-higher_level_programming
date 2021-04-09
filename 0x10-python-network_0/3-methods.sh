63429502a159e0046c50ede595d1cc4634bd74d0
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-04-08 22:01:50 -0500

    First version

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that takes in a URL and displays all HTTP methods the server accepts
-curl -sI localhost:5000/route_4 | grep "Allow" | cut -d' ' -f2-  
+curl -sI "$1"/route_4 | grep "Allow" | sed 's/^Allow: //'    
