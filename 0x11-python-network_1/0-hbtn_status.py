000489ba6de0566fe92999cd36c9289f267f0120
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-04-09 15:51:44 -0500

    First version

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """a Python script that fetches https://intranet.hbtn.io/status"""
 if __name__ == "__main__":
-    from urllib import request
-    with request.urlopen('https://intranet.hbtn.io/status') as rq:
+    import urllib.request as req
+    with req.urlopen('https://intranet.hbtn.io/status') as rq:
+        status = rq.read()
         print("Body response:")
-        print("\t- type: {}".format(type(rq.peek())))
-        print("\t- content: {}".format(rq.peek()))
-        print("\t- utf8 content: {}".format(rq.peek().decode('utf-8')))
+        print("\t- type: {}".format(type(status)))
+        print("\t- content: {}".format(status))
+        print("\t- utf8 content: {}".format(status.decode('utf-8')))
