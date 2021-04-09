fee7fc9038b7e86c8d027bde2b877ad6e8b77486
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-04-08 19:09:41 -0500

    Fixing for length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL, and displays
+# A Bash script that takes in a URL and displays the size of the body
 curl -Is "$1" | grep 'Content-Length' | awk 'NF>1{print $NF}'
