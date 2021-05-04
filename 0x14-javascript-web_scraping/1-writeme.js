40433dc54548ef2be60c24ad547301201c524c14
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-05-04 12:36:06 -0500

    First version

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const FileName = process.argv[2];
+const FileContent = process.argv[3];
+fs.writeFile(FileName, FileContent, 'utf-8', (err) => {
+  if (err) throw err;
+  console.log('The file has been saved');
+});
