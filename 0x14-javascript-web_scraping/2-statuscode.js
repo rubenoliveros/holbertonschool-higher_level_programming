4d7a2a4b4f5bcd6bc07a8081901e5913ca98032f
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-05-04 20:25:04 -0500

    First version

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const UrlToGet = process.argv[2];
-request(UrlToGet, function (response) {
+request(UrlToGet, function (err, response) {
+  if (err) throw err;
   console.log('code: ', response && response.statusCode);
 });
