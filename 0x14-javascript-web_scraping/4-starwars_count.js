8e2795a022f97eef45ecd21abd710dba7a0759e6
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-05-04 22:09:55 -0500

    First version

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const UrlToGet = process.argv[2];
-const WedgeAntilles = 18;
+const WedgeAntillesUrl = 'https://swapi-api.hbtn.io/api/people/18/';
 request(UrlToGet, function (err, response, body) {
   if (err) throw err;
   if (response.statusCode === 200) {
     let WedgeAntillesCount = 0;
     const films = JSON.parse(body).results;
     for (const i in films) {
-      if (films[i].characters.includes(`https://swapi-api.hbtn.io/api/people/${WedgeAntilles}/`)) {
+      if (films[i].characters.includes(WedgeAntillesUrl)) {
         WedgeAntillesCount++;
       }
     }
